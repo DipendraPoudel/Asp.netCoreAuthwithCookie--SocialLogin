3525c3105b221941399d5452555e1ea9a68be6a2
dipendra <dipendra123p@yahoo.com>
2021-07-31 10:42:12 +0930

    controller,models created

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using AuthwithCookieSocialLogin.Models;
+﻿using AuthwithCookieSocialLogin.Data;
+using AuthwithCookieSocialLogin.Models;
+using AuthwithCookieSocialLogin.Models.SchoolViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,10 +15,12 @@ namespace AuthwithCookieSocialLogin.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly SchoolContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, SchoolContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -23,6 +28,19 @@ namespace AuthwithCookieSocialLogin.Controllers
             return View();
         }
 
+
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
         public IActionResult Privacy()
         {
             return View();
